2101d6d082c1cc85bdcb441aaf5c37d0c1a7db45
admin <admin@admin.com>
2021-11-30 08:37:39 +0600

    Implementation of control panel and displaying cubes with numbers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouletteService } from './shared/roulette.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  numbers: number[] = [];
+  isStarted = false;
+
+  constructor(
+    private rouletteService: RouletteService,
+  ) {}
+
+  ngOnInit() {
+    this.rouletteService.newNumber.subscribe(number => {
+      this.numbers.push(number);
+    });
+  }
+
+  start() {
+    if (this.isStarted) {
+      return;
+    }
+
+    this.rouletteService.start();
+    this.isStarted = true;
+  }
+
+  stop() {
+    this.rouletteService.stop();
+    this.isStarted = false;
+  }
+
+  reset() {
+    this.numbers = [];
+    this.stop();
+  }
+}
